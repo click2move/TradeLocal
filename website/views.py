ed719732ae42df6906b731726f207dd19d7060a0
Goldammer2003 <mangobo@freenet.de>
2025-01-19 17:29:43 +0100

    Überarbeitung der Navigationsleiste innerhalb des base.html files mitsamt Überarbietung der Routes über Neuanlage im views.py file

--- website/views.py
+++ website/views.py
@@ -1,32 +1,46 @@
 from flask import Blueprint, render_template, request, flash, jsonify
-from flask_login import login_required, current_user
-from .models import Note
+from flask_login import current_user
+from .models import Note  # Modell für Notizen
 from . import db
 import json
 
 views = Blueprint('views', __name__)
 
-
+# Home-Route: Produktübersicht
 @views.route('/', methods=['GET', 'POST'])
-@login_required
 def home():
     if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
+        note = request.form.get('note')  # Für Notizen (falls weiterhin benötigt)
 
         if len(note) < 1:
             flash('Note is too short!', category='error') 
         else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
+            new_note = Note(data=note, user_id=current_user.id)  # Note speichern
+            db.session.add(new_note)
             db.session.commit()
             flash('Note added!', category='success')
 
     return render_template("home.html", user=current_user)
 
+# Warenkorb-Route
+@views.route('/warenkorb')
+def cart():
+    return render_template('Warenkorb.html', user=current_user)
 
+# Aktiv-Route
+@views.route('/aktiv')
+def aktiv():
+    return render_template('aktiv.html', user=current_user)
+
+# Kontakt-Route
+@views.route('/kontakt')
+def kontakt():
+    return render_template('kontakt2.html', user=current_user)
+
+# Route für das Löschen von Notizen (falls benötigt)
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    note = json.loads(request.data)  # JSON-Daten verarbeiten
     noteId = note['noteId']
     note = Note.query.get(noteId)
     if note:
@@ -35,3 +49,9 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
+
+# Dummy-Route für zukünftige Produktdetails
+@views.route('/produkt/<int:produkt_id>')
+def produkt_detail(produkt_id):
+    # Beispiel: Lade später dynamische Inhalte basierend auf der Produkt-ID
+    return render_template('produkt_detail.html', produkt_id=produkt_id)
